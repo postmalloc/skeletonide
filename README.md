0d9ce587e1fab40fd7d0854a7b4e38a3b67ca1af
Srimukh Sripada <hi@srimukh.com>
2020-10-13 00:55:25 +0530

    single pass skeletonization works

--- README.md
+++ README.md
@@ -3,7 +3,13 @@ Skeletonide is a parallel implementaion of Zhang-Suen morphological
 thinning algorithm written in Halide-lang. It can be used for fast
 skeletonization of binary masks.
 
-# Build
+Status: It can only run one pass at a time 
+natively in halide.  
+
+TODO: Either support multiple passes in halide,
+or offload that to an extern C function.
+
+## Build
 ```
 mkdir build
 cd build
@@ -11,5 +17,5 @@ cmake ..
 cmake --build .
 ```
 
-# License
+## License
 MIT License
--- main.cpp
+++ main.cpp
@@ -2,40 +2,79 @@
 #include "halide_image_io.h"
 
 using namespace Halide;
-int main(int argc, char **argv){
-    Buffer<float> input = Tools::load_and_convert_image("images/horse.png");
+
+// TODO support multiple passes natively in Halide.
+// TODO or, provide an extern C function to iterate over passes
+int main(int argc, char **argv) {
+    Buffer<uint8_t> input = Tools::load_and_convert_image("test/images/horse.png");
     int W = input.width();
     int H = input.height();
+    Buffer<uint8_t> output(W, H);
 
-    Buffer<bool> one2zero(W, H);
-
-    int n_x_idx[8] = {-1,-1,0,1,1,1,0,-1};
-    int n_y_idx[8] = {0,1,1,1,0,-1,-1,-1};
-    
+    // offsets of nbrbours of a given pixel (x, y)
+    int n_x_idx[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+    int n_y_idx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
     Buffer<int> n_x_idx_buf(n_x_idx);
     Buffer<int> n_y_idx_buf(n_y_idx);
+
     Var x, y, k;
     Func in_bounded = BoundaryConditions::repeat_edge(input);
-    Func erase;
 
-    Func neigh;
-    neigh(x, y, k) = in_bounded(x+n_x_idx_buf(k), y+n_y_idx_buf(k));
-    
+    // fn to get the nth nbrbour of a pixel
+    Func nbr;
+    nbr(x, y, k) = in_bounded(x + n_x_idx_buf(k), y + n_y_idx_buf(k));
+
+    // fn count the number of non-zero nbrbors
     RDom j(0, 8);
-    Func nsum;
-    nsum(x, y) = sum(neigh(x, y, j));
+    Func nbr_cnt("nbr_cnt");
+    nbr_cnt(x, y) = sum(select(nbr(x, y, j) > 0, 1, 0));
 
-    Func zero2one;
-    Expr zero2one_cnt;
+    // fn to count the number of transitions
+    Func zero2one("zero2one");
     RDom t(0, 8);
-    Expr zero2one_cond = neigh(x, y, t%8) == 0 && neigh(x, y, (t+1)%8) == 1;
+    Expr zero2one_cond = nbr(x, y, t % 8) == 0 && nbr(x, y, (t + 1) % 8) == 255;
     zero2one(x, y) = sum(select(zero2one_cond, 1, 0));
 
-//    nsum.compute_root();
-//    nsum.trace_stores();
-//    nsum.realize(H, W);
-    zero2one.trace_stores();
-    zero2one.compute_root();
-    zero2one.realize(H, W);
+    // step-1 of Zhang-Suen method
+    Expr fst_cnd;
+    fst_cnd = (nbr_cnt(x, y) >= 2 && nbr_cnt(x, y) <= 6 && zero2one(x, y) == 1 &&
+               nbr(x, y, 0) * nbr(x, y, 2) * nbr(x, y, 4) == 0 &&
+               nbr(x, y, 0) * nbr(x, y, 2) * nbr(x, y, 6) == 0);
+
+    Func skel1("skel1");
+    skel1(x, y) = cast<uint8_t>(select(fst_cnd, 255, in_bounded(x, y)));
+    skel1.compute_root().vectorize(x, 8).parallel(y);
+
+    // step-2 of Zhang-Suen method
+    // operate on the array modified in step-1
+    Func nbr2;
+    nbr2(x, y, k) =
+            skel1(clamp(x + n_x_idx_buf(k), 0, W), clamp(y + n_y_idx_buf(k), 0, H));
+
+    RDom j2(0, 8);
+    Func nbr_cnt2("nbr_cnt2");
+    nbr_cnt2(x, y) = sum(select(nbr2(x, y, j2) > 0, 1, 0));
+
+    Func zero2one2("zero2one2");
+    RDom t2(0, 8);
+    Expr zero2one_cond2 =
+            nbr2(x, y, t2 % 8) == 0 && nbr2(x, y, (t2 + 1) % 8) == 255;
+    zero2one2(x, y) = sum(select(zero2one_cond2, 1, 0));
+
+    Expr snd_cnd;
+    snd_cnd =
+            (nbr_cnt2(x, y) >= 2 && nbr_cnt2(x, y) <= 6 && zero2one2(x, y) == 1 &&
+             nbr2(x, y, 0) * nbr2(x, y, 4) * nbr2(x, y, 6) == 0 &&
+             nbr2(x, y, 2) * nbr2(x, y, 4) * nbr2(x, y, 6) == 0);
+
+    Func skel2("skel2");
+    skel2(x, y) = cast<uint8_t>(select(snd_cnd, 255, skel1(x, y)));
+    skel2.compute_root().vectorize(x, 8).parallel(y);
+
+    Func res;
+    res(x, y) = skel2(x, y);
+    res.trace_stores();
+    res.realize(output);
+    Tools::convert_and_save_image(output, "test/output/skel.png");
     return 0;
 }
